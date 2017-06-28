12845a022d60b3fcc4488a37a14075018a2c97c9
Jay Deverett <jayjdeverett@gmail.com>
2017-06-28 15:21:04 -0400

    add first exercise

---
+++ exercise.rb
@@ -0,0 +1,43 @@
+fav_colors = ["red", "blue", "yellow", "orange", "green"]
+
+ages = [27, 28, 29, 40, 41, 42]
+
+coin_toss = ["yes", "yes", "yes", "no", "yes"]
+
+fav_artists = ["Jack White", "Jay Z", "Oasis"]
+
+color_sym = [:red, :blue, :yellow, :orange, :green]
+
+word_defs = {
+  :concatenate => "link things together in a chain or series.",
+  :chill => "calm down and relax.",
+  :dog => "act lazily; fail to try one's hardest."
+}
+
+fav_movies = {
+  fight_club: 1999,
+  kick_ass: 2010,
+  the_matrix: 1999
+}
+
+city_pop = {
+  dubai: 2789000,
+  singapore: 5535000,
+  berlin: 3500000
+}
+
+cousin_ages = {
+  chris: 27,
+  alex: 28,
+  sally: 40
+}
+
+puts coin_toss
+
+puts fav_colors[0]
+
+puts [ages.sort]
+
+puts ages.push[0]
+
+puts fav_movies[:the_matrix]
